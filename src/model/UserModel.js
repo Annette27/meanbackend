1aeb931d94ca863a365e0aec91cd80a6155b2a25
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-09-22 12:17:08 +0530

    first commit

---
+++ src/model/UserModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+mongoose.connect('mongodb://localhost:27017/LIBRARYAPPNEW')
+
+// mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/LIBRARYAPPNEW?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    exampleInputEmail1:String,
+    exampleInputPassword1:String,
+  });
+var Userdata = mongoose.model('userdatadata',UserSchema);
+module.exports = Userdata;
